662acc1b26172dc3c37e98df5b82fae68720da2a
Juan Luis de Sousa-Valadas Castaño <jdesousa@redhat.com>
2018-02-22 11:56:28 +0100

    make the script copy from etcd2 to etcd2

--- main.go
+++ main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
-	//"os"
 	"time"
 
 	"github.com/coreos/etcd/client"
-	"github.com/coreos/etcd/clientv3"
 	"github.com/coreos/etcd/pkg/transport"
 )
 
@@ -20,95 +17,70 @@ type etcdFlags struct {
 	caFile      *string
 }
 
-func generateV3ClientConfig(flags *etcdFlags) (*clientv3.Config, error) {
-
-	c := &clientv3.Config{
-		Endpoints: []string{*(flags.etcdAddress)},
-	}
-
-	tlsinfo := transport.TLSInfo{
-		CertFile: *(flags.certFile),
-		KeyFile:  *(flags.keyFile),
-		CAFile:   *(flags.caFile),
-	}
-
-	clientTLS, err := tlsinfo.ClientConfig()
-	if err != nil {
-		return nil, fmt.Errorf("Error while creating etcd client: %v", err)
-	}
-	c.TLS = clientTLS
-	return c, nil
-}
-
-func wipeEtcd3(v3Config clientv3.Config) error {
-	c, err := clientv3.New(v3Config)
-	if err != nil {
-		return err
-	}
-	defer c.Close()
-
-	_, err = c.Delete(context.Background(), "/", clientv3.WithFromKey())
-	if err != nil {
-		return err
-	}
-	log.Print("Successfully wiped etcd3")
-	return nil
-}
-
-func createEtcdNodeRecursive(c clientv3.Client, nodes client.Nodes) error {
+func createEtcdNodeRecursive(k client.KeysAPI, nodes client.Nodes) error {
 	for _, node := range nodes {
 		if node.Dir {
-			err := createEtcdNodeRecursive(c, node.Nodes)
+			log.Println("Iterating over key: " + node.Key)
+			_, err := k.Set(context.Background(), node.Key, "", &client.SetOptions{TTL: node.TTLDuration(), Dir: true})
+			if err != nil {
+				return err
+			}
+			err = createEtcdNodeRecursive(k, node.Nodes)
 			if err != nil {
 				return err
 			}
 		} else {
-			_, err := c.Put(context.Background(), node.Key, node.Value)
+
+			_, err := k.Create(context.Background(), node.Key, node.Value)
 			if err != nil {
 				return err
 			}
-			log.Print("Successfully written key: " + node.Key)
+			log.Println("Successfully written key: " + node.Key)
 		}
 	}
 	return nil
 }
 
-func writeEtcd3Data(v3Config clientv3.Config, nodes client.Nodes) error {
-	c, err := clientv3.New(v3Config)
-	if err != nil {
-		return err
-	}
-	defer c.Close()
-	err = createEtcdNodeRecursive(*c, nodes)
+func writeEtcdData(k client.KeysAPI, nodes client.Nodes) error {
+	err := createEtcdNodeRecursive(k, nodes)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func generateV2ClientConfig(flags *etcdFlags) (client.Client, error) {
-	tlsInfo := transport.TLSInfo{
-		CertFile: *(flags.certFile),
-		KeyFile:  *(flags.keyFile),
-		CAFile:   *(flags.caFile),
-	}
+func generateV2Client(flags *etcdFlags) (client.KeysAPI, error) {
 
-	tr, err := transport.NewTransport(tlsInfo, 30*time.Second)
-	if err != nil {
-		return nil, err
-	}
 	cfg := client.Config{
-		Transport:               tr,
 		Endpoints:               []string{*(flags.etcdAddress)},
 		HeaderTimeoutPerRequest: 30 * time.Second,
 	}
 
-	return client.New(cfg)
+	if *(flags.certFile) != "" || *(flags.keyFile) != "" || *(flags.caFile) != "" {
+		tlsInfo := transport.TLSInfo{
+			CertFile: *(flags.certFile),
+			KeyFile:  *(flags.keyFile),
+			CAFile:   *(flags.caFile),
+		}
+
+		tr, err := transport.NewTransport(tlsInfo, 30*time.Second)
+		if err != nil {
+			return nil, err
+		}
+		cfg.Transport = tr
+	}
+
+	c, err := client.New(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.NewKeysAPI(c), nil
+
 }
 
-func getV2Keys(c client.Client) (client.Nodes, error) {
-	kapiV2 := client.NewKeysAPI(c)
-	resp, err := kapiV2.Get(context.Background(), "/", &client.GetOptions{Sort: true, Recursive: true, Quorum: true})
+func getV2Keys(k client.KeysAPI) (client.Nodes, error) {
+	resp, err := k.Get(context.Background(), "/", &client.GetOptions{Sort: true, Recursive: true, Quorum: true})
 	if err != nil {
 		return nil, err
 	}
@@ -117,37 +89,45 @@ func getV2Keys(c client.Client) (client.Nodes, error) {
 }
 
 func main() {
-	flags := &etcdFlags{
-		etcdAddress: flag.String("etcd-address", "", "Etcd address"),
-		certFile:    flag.String("cert", "", "identify secure client using this TLS certificate file"),
-		keyFile:     flag.String("key", "", "identify secure client using this TLS key file"),
-		caFile:      flag.String("cacert", "", "verify certificates of TLS-enabled secure servers using this CA bundle"),
+	destFlags := &etcdFlags{
+		etcdAddress: flag.String("dest-etcd-address", "", "Etcd address"),
+		certFile:    flag.String("dest-cert", "", "identify secure client using this TLS certificate file"),
+		keyFile:     flag.String("dest-key", "", "identify secure client using this TLS key file"),
+		caFile:      flag.String("dest-cacert", "", "verify certificates of TLS-enabled secure servers using this CA bundle"),
+	}
+
+	srcFlags := &etcdFlags{
+		etcdAddress: flag.String("src-etcd-address", "", "Etcd address"),
+		certFile:    flag.String("src-cert", "", "identify secure client using this TLS certificate file"),
+		keyFile:     flag.String("src-key", "", "identify secure client using this TLS key file"),
+		caFile:      flag.String("src-cacert", "", "verify certificates of TLS-enabled secure servers using this CA bundle"),
 	}
 
 	flag.Parse()
-	if *(flags.etcdAddress) == "" || *(flags.certFile) == "" || *(flags.keyFile) == "" || *(flags.caFile) == "" {
-		log.Fatal("--etcd-address --cert --key and --cacert are required")
+	if *(srcFlags.etcdAddress) == "" || *(srcFlags.certFile) == "" || *(srcFlags.keyFile) == "" || *(srcFlags.caFile) == "" {
+		log.Fatal("--src-etcd-address --src-cert --src-key and --cacert are required")
+	}
+	if *(destFlags.etcdAddress) == "" {
+		log.Fatal("--dest-etcd-address is required")
 	}
 
-	v2Client, err := generateV2ClientConfig(flags)
+	srcClient, err := generateV2Client(srcFlags)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	v3ClientConfig, err := generateV3ClientConfig(flags)
+	destClient, err := generateV2Client(destFlags)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Already initialized
-	wipeEtcd3(*v3ClientConfig)
 
 	// fetch v2 Keys
-	nodes, err := getV2Keys(v2Client)
+	nodes, err := getV2Keys(srcClient)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// create them in v3
-	writeEtcd3Data(*v3ClientConfig, nodes)
+	log.Println(nodes)
+	err = writeEtcdData(destClient, nodes)
 	if err != nil {
 		log.Fatal(err)
 	}
